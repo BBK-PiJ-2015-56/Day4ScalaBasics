415eb5503d3fb48a4f67b35be87ac3d639f7912e
pdomok01 <pdomok01@mail.bbk.ac.uk>
2016-02-25 13:26:44 +0000

    q1 worked out why head and tail were not seen

--- Day8/src/q1/LinkedList.scala
+++ Day8/src/q1/LinkedList.scala
@@ -32,24 +32,34 @@ final case object End extends LinkedList[Nothing]{def length = 0}
 final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]{ def length = {}
 }
 */
-//AP: do the same as above but with the length method in a companion object
+//AP: I will do the same as above but with the length method in a companion object,
+//just to ensure avoidance of the boredom that a world without variety brings
 sealed trait LinkedList[+A] // had to make this covariant so it accepted List[nothing]
 final case object End extends LinkedList[Nothing]
 final case class Pair[A](val head: A, val tail: LinkedList[A]) extends LinkedList[A]
 
 object LinkedList{
+  //Q) does every method need to be parameterised by A or +A?
   def length[A] (as:LinkedList[A]):Int = as match{
     case End => 0
     case Pair(h,t) => 1 + length(t)
   }
-  //def tailRecLength[A] (as:LinkedList[A] , acc:Int):Int = {
-  //  case End => acc
-   // case Pair(h,t) => tailRecLength(t , acc + 1)
-  //}
-  def contains[A](as:LinkedList[A] , a:A): Boolean = {
+  def tailRecLength[A] (as:LinkedList[A] , acc:Int):Int = as match {
+    case End => acc
+    case Pair(h,t) => tailRecLength(t , acc + 1)
+  }
+  def contains[A](as:LinkedList[A], a:A): Boolean = as match{
     case End => false
     case Pair(a,t) => true
-    case (h,t) => contains(t,a)
+    case Pair(h,t) => contains(t,a)
+  }
+  //Q) why is this called 'apply'
+  // ie why would we want to call this method automatically?
+  //Q) seeing as I ma doing it throuygh a companion object,
+  // does it still make sense to call it 'apply'?
+  def apply[A](as:LinkedList[A] , n:Int):A = n match{
+    case 1 => as.head
+    case x if x > 0 => LinkedList.apply(as , n-1)
   }
 }
 
@@ -57,30 +67,49 @@ object TestLinkedList extends App {
   val list: LinkedList[Int] = Pair(1, Pair(2, End))
   println("list has length " + LinkedList.length(list))
   LinkedList.length(list) is 2
-  println(list)
-  //println(list.head)
+  println("The LinkedList called 'list' is:   " + list)
+  println("The head of list is   " + list.head)
+  //println("trying to print the head of list, which is 1...   " + list.head)
   //Q) why doesn't list.head and list.tail work??
 
   val example: LinkedList[Int] = Pair(1, Pair(2, Pair(3, End)))
   LinkedList.length(example) is 3
   //assert(LinkedList.length(example.tail) == 2)
   assert(LinkedList.length(End) == 0)
-  println(LinkedList.length(End))
-}
+  println("The length of End is:  " + LinkedList.length(End))
+
 
   //AP: test tail-recursive length
-  //println("list has length " + LinkedList.tailRecLength(list, 0))
-  //LinkedList.tailRecLength(list,0) is 2
-  // println(list)
-  /* Q) runtime Error:(44, 59) missing parameter type for expanded function
-  The argument types of an anonymous function must be fully known. (SLS 8.5)
-  Expected type was: Int
-  def tailRecLength[A] (as:LinkedList[A] , acc:Int):Int = {
-  ^*/
+  println("list has tail-rec length " + LinkedList.tailRecLength(list, 0))
+  LinkedList.tailRecLength(list, 0) is 2
 
 
-  //assert(example.contains(3) == true)
-  //assert(example.contains(4) == false)
-  //assert(Empty().contains(0) == false)
-  // This should not compile
-  // example.contains("not an Int")
+//test contains
+  println( "trying contains 2, so should be true...")
+  println(LinkedList.contains[Int](example,2)) // note specifying Int here is optional
+  assert(LinkedList.contains(example,2) == true)
+
+  println( "trying contains 9, so should be false...")
+  println(LinkedList.contains(example,9))
+  //assert(LinkedList.contains(example,9) == false)
+  //Q) why is it still true???
+  println("The following should not compile...")
+ // println(LinkedList.contains[Int](example , "not an Int")) Correct doesn't compile, unless I dont specify Int
+  //If I didnt specify the param to be Int, then the compiler treats A to be a String.
+  //Q) Should I change it so it doesn't compile in any case???
+
+
+//testing part e
+  /*
+  assert(example(0) == 1)
+  assert(example(1) == 2)
+  assert(example(2) == 3)
+  assert(try {
+    example(3)
+    false
+  } catch {
+    case e: Exception => true
+  })
+   */
+
+}
---
+++ Day8/src/q1/q1HeadAndTail.sc
@@ -0,0 +1,14 @@
+sealed trait LinkedList1[+A] // had to make this covariant so it accepted List[nothing]
+final case object End1 extends LinkedList1[Nothing]
+final case class Pair1[A](val head: A, val tail: LinkedList1[A]) extends LinkedList1[A]
+val list1: LinkedList1[Int] = Pair1(1, Pair1(2, End1))
+println(list1)
+//println("The head of list1 is: " + list1.head)
+
+val list2:Pair1[Int] = Pair1(1, Pair1(2, Pair1(3 , End1)))
+println(list2)
+println("The head of list2 is: " + list2.head)
+
+
+// So if you declare it as a LinkedList then it cannot see Pair, because it doesn't know about it.
+// You need to cast it to Pair
