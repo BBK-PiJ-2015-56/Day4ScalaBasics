357d33d6cea99d94d9a8e4b030b39cfa7605906d
Peter Domokos <pdomok01@mail.bbk.ac.uk>
2016-03-01 14:25:19 +0000

    q4,5

--- Day8/src/q4and5tailrecursion/Factorial.scala
+++ Day8/src/q4and5tailrecursion/Factorial.scala
@@ -5,7 +5,7 @@ def factorial(n: Int): Int = if (n == 0) 1 else n * factorial(n - 1)
 Design a tail-recursive version of factorial.
 */
 
-object FactorialTest extends App {
+object FactorialTest  {
   def factorial(n: Int , acc:Int):Int = n match{
     case n if n == 0 => acc
     case _ => factorial(n-1 , acc * n )
@@ -28,24 +28,30 @@ iter(??, ??)
 
 
 
-
-
-
-
-
-
-
-
-
-/*
-
-
 object SumTest extends App {
-
-  def sum(f: Int => Int)(a: Int, b: Int): Int = match a {
-    def iter = f(a)
-    case b => 0
-    case _ => sum(f)(a + 1 , b)
+  //QQQQQ) the pattern match method is just returning acc!??????????????
+  def sumWithPatternMatch(f: Int => Int)(a: Int, b: Int): Int = {
+    //iter will compute f(x) for every x and add it to teh accumulator, then return acc when x = b
+    def iter(x: Int, acc: Int): Int = x + 1 match {
+      case b => acc
+      case _ => iter(x + 1, acc + f(x))
+    }
+    iter(a , 0)
   }
+
+  def sumWithIf(f: Int => Int)(a: Int, b: Int): Int = {
+    //iter will compute f(x) for every x and add it to teh accumulator, then return acc when x = b
+    def iter(x: Int, acc: Int): Int = {
+      if(x == b + 1) acc
+      else iter(x + 1, acc + f(x))
+    }
+    iter(a , 0)
+  }
+  def double(x:Int) = 2*x
+  println(double(3) + double(4) + double(5))
+  println("testing sumWithPatternMatch")
+  println(sumWithPatternMatch(double)(3 , 5))
+  println("testing sumWithIf")
+  println(sumWithIf(double)(3 , 5))
+
 }
-*/
